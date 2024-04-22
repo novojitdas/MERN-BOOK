2976bd4c8b538ccec842944f98c2501bf064a2e8
novojitdas <novojitdas@gmail.com>
2024-04-22 15:35:06 +0600

    fixed index.js cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { PORT, MONGODB } from "./config.js";
 import { Book } from "./models/bookModel.js";
 import bookRoute from "./routes/bookRoute.js";
-
+import cors from "cors";
 const app = express();
 
 ////////////////
@@ -14,15 +14,15 @@ app.use(express.json());
 
 //middleware for handling cors policy
 //option 1: allow all cors with default of all cors (*)
-// app.use(cors());
+app.use(cors());
 // option 2: allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowHeaders: ["Content-Type"],
+//   })
+// );
 
 /////////////
 // Route //
@@ -31,7 +31,8 @@ app.use("/books", bookRoute);
 
 app.get("/", (req, res) => {
   res.status(234);
-  res.redirect("/books");
+  // res.redirect("/books");
+  res.send("Welcome to MERN-BookStore Project");
 });
 
 //////////////
